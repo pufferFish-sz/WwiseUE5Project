dd4096c8d8ad8666f351a187c5137406922e2792
Shangyi Zhu <62895460+pufferFish-sz@users.noreply.github.com>
2024-09-19 23:52:42 -0400

    Character Included2

---
+++ WwiseUE5/Intermediate/Build/Win64/UnrealEditor/Inc/WwiseUE5/UHT/WwiseEvent.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "WwiseEvent.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef WWISEUE5_WwiseEvent_generated_h
+#error "WwiseEvent.generated.h already included, missing '#pragma once' in WwiseEvent.h"
+#endif
+#define WWISEUE5_WwiseEvent_generated_h
+
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_SPARSE_DATA
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_ACCESSORS
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUWwiseEvent(); \
+	friend struct Z_Construct_UClass_UWwiseEvent_Statics; \
+public: \
+	DECLARE_CLASS(UWwiseEvent, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/WwiseUE5"), NO_API) \
+	DECLARE_SERIALIZER(UWwiseEvent)
+
+
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UWwiseEvent(UWwiseEvent&&); \
+	NO_API UWwiseEvent(const UWwiseEvent&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UWwiseEvent); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UWwiseEvent); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UWwiseEvent) \
+	NO_API virtual ~UWwiseEvent();
+
+
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_11_PROLOG
+#define FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_SPARSE_DATA \
+	FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_ACCESSORS \
+	FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_INCLASS_NO_PURE_DECLS \
+	FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> WWISEUE5_API UClass* StaticClass<class UWwiseEvent>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_shang_OneDrive_Documents_Unreal_Projects_WwiseUE5Project_WwiseUE5_Source_WwiseUE5_Public_WwiseEvent_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
