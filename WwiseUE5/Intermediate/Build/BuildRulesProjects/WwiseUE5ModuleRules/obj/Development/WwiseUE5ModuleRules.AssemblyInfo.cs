5971b18be4796d65f19fab929adf7501df57fc28
Shangyi Zhu <62895460+pufferFish-sz@users.noreply.github.com>
2024-09-20 12:03:36 -0400

    Footstep Sounds Added

--- WwiseUE5/Intermediate/Build/BuildRulesProjects/WwiseUE5ModuleRules/obj/Development/WwiseUE5ModuleRules.AssemblyInfo.cs
+++ WwiseUE5/Intermediate/Build/BuildRulesProjects/WwiseUE5ModuleRules/obj/Development/WwiseUE5ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WwiseUE5ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99e7473cbb51f324425f5d859ae93a705a14f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("WwiseUE5ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WwiseUE5ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
